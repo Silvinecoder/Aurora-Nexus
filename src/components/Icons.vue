b7e72b966578ad10c6456af0811b307188f1199f
pati167 <pgomes1404@gmail.com>
2024-03-19 17:30:26 +0000

    adding icons and create img component

---
+++ src/components/Icons.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="gastronony_images">
+    <img src="" alt="" />
+    <p></p>
+  </div>
+  <svg></svg>
+</template>
+
+<script>
+export default{
+    data(){
+        gastronony_images(){
+            console.log("gastronomy");
+        },
+    }
+}
+</script>
