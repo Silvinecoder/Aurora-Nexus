1aa3a87c2f1a89811b3ea379bd6d5404dbad188b
silvinecoder <creativeservicepackages@gmail.com>
2024-05-24 17:24:07 +0100

    adding into mono

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -6,18 +6,15 @@
 
   <div class="products__body">
     <div class="carousel__container">
-      <Carousel :imageUrl="'/icons/supermarkets/lidl_3x.png'" :imageAlt="'Lidl Logo'" />
-      <Carousel :imageUrl="'/icons/supermarkets/aldi_3x.png'" :imageAlt="'Aldi Logo'" />
-      <Carousel :imageUrl="'/icons/supermarkets/continente_3x.png'" :imageAlt="'Continente Logo'" />
-      <Carousel :imageUrl="'/icons/supermarkets/minipreco_3x.png'" :imageAlt="'Minipreco Logo'" />
-      <Carousel :imageUrl="'/icons/supermarkets/pingoDoce_3x.png'" :imageAlt="'Pingo Doce Logo'" />
+      <Carousel v-for="supermarket in supermarkets" :key="supermarket.id" :imageUrl="supermarket.imageUrl"
+        :imageAlt="supermarket.alt" />
     </div>
 
     <div class="image_cards__category_container" v-for="(category, categoryName) in categories" :key="categoryName">
       <CategoryTitle :categoryName="categoryName" :goToCategory="goToCategory" :showButton="true" />
       <div class="image_cards_container">
         <Card v-for="product in category.products" :key="product.product_uuid" :product="product"
-          :addToCart="addToCart" />
+          :addToCart="addToCart" :removeFromCart="removeFromCart"/>
       </div>
     </div>
   </div>
@@ -34,16 +31,29 @@ import Card from "../components/Card.vue";
 export default {
   mixins: [categoryAndProductsMixin],
   components: { Buttons, Search, Carousel, Card, CategoryTitle },
+  data() {
+    return {
+      categoryName: '', // Initialize categoryName
+      supermarkets: [
+        { id: 1, imageUrl: '/icons/supermarkets/lidl_3x.png', alt: 'Lidl Logo' },
+        { id: 2, imageUrl: '/icons/supermarkets/aldi_3x.png', alt: 'Aldi Logo' },
+        { id: 3, imageUrl: '/icons/supermarkets/continente_3x.png', alt: 'Continente Logo' },
+        { id: 4, imageUrl: '/icons/supermarkets/minipreco_3x.png', alt: 'Minipreço Logo' },
+        { id: 5, imageUrl: '/icons/supermarkets/pingoDoce_3x.png', alt: 'Pingo Doce Logo' }
+      ],
+    };
+  },
   created() {
     this.fetchCategoriesAndProducts();
   },
   methods: {
     addToCart(product) {
-      let cart = JSON.parse(localStorage.getItem('cart')) || [];
-      cart.push(product);
-      localStorage.setItem('cart', JSON.stringify(cart));
-      console.log('cart', cart);
-      console.log('Product added to cart:', product);
+      this.$store.dispatch('addToCart', product.product_uuid);
+      console.log('Add to cart button clicked');
+    },
+    removeFromCart(product) {
+      this.$store.dispatch('removeFromCart', product.product_uuid);
+      console.log('Remove from cart button clicked');
     },
     goToCategory(categoryName) {
       this.$router.push({ name: 'Category', params: { categoryName: categoryName } });
