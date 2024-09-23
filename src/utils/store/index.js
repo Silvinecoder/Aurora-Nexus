e56d8b59532ce21ffe245264976373a4ea4a0d3a
silvinecoder <creativeservicepackages@gmail.com>
2024-09-23 15:19:53 +0100

    fixing stuff

--- src/utils/store/index.js
+++ src/utils/store/index.js
@@ -5,13 +5,15 @@ export default createStore({
     cart: [],
     selectedCategory: "",
     selectedSupermarket: null,
+    groupedProducts: {},
+    supermarketState: {},
+    allProducts: []
   },
   mutations: {
     addToCart(state, product) {
       const isProductInCart = state.cart.some(
         (item) => item.product_uuid === product.product_uuid
       );
-  
       if (!isProductInCart) {
         state.cart.push(product);
       }
@@ -30,13 +32,24 @@ export default createStore({
     setSelectedSupermarket(state, supermarket) {
       state.selectedSupermarket = supermarket;
     },
+    updateGroupedProducts(state, groupedProducts) {
+      state.groupedProducts = groupedProducts;
+    },
+    updateSupermarketState(state, supermarketState) {
+      state.supermarketState = supermarketState;
+    },
+    updateAllProducts(state, products) {
+      state.allProducts = products;
+    },
   },
   actions: {
-    addToCart({ commit }, product) {
+    addToCart({ commit, dispatch }, product) {
       commit("addToCart", product);
+      dispatch("groupProductsBySupermarket", this.state.allProducts);
     },
-    removeFromCart({ commit }, product) {
+    removeFromCart({ commit, dispatch }, product) {
       commit("removeFromCart", product);
+      dispatch("groupProductsBySupermarket", this.allProducts);
     },
     clearCart({ commit }) {
       commit("clearCart");
@@ -47,6 +60,24 @@ export default createStore({
     updateSelectedSupermarket({ commit }, supermarket) {
       commit("setSelectedSupermarket", supermarket);
     },
+    groupProductsBySupermarket({ commit, getters }, allProducts) {
+      const groupedProducts = {};
+      const supermarketState = {};
+    
+      allProducts
+        .filter(product => getters.isProductInCart(product.product_uuid))
+        .forEach(product => {
+          const supermarketUUIDs = product.supermarket_uuids || product.supermarket_uuid;
+          const uuids = Array.isArray(supermarketUUIDs) ? supermarketUUIDs : [supermarketUUIDs];
+          uuids.forEach(uuid => {
+            if (!groupedProducts[uuid]) groupedProducts[uuid] = [];
+            groupedProducts[uuid].push(product);
+          });
+        });
+    
+      commit("updateGroupedProducts", groupedProducts);
+      commit("updateSupermarketState", supermarketState);
+    }
   },
   getters: {
     getSelectedSupermarket: (state) => state.selectedSupermarket,
@@ -54,5 +85,8 @@ export default createStore({
     isProductInCart: (state) => (product_uuid) => {
       return state.cart.some(item => item.product_uuid === product_uuid);
     },
-  },
+    cartIsEmpty: (state) => {
+      return state.cart.length === 0;
+    }
+  }
 });
